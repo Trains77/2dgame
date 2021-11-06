ed128c176f7419e665f9a059770578eb943ad066
Trains77 <trains77videos123@gmail.com>
2021-11-06 11:30:20 -0400

    Improved movement

--- game/game.py
+++ game/game.py
@@ -10,7 +10,7 @@ from colored import fore, back, style
 import math
 import random
 import credits
-from shared import size, GameName, fps, background_color, BLACK, RED, GREEN, BLUE, WHITE, GRAY, player_color, square_size, speed, jump
+from shared import size, GameName, gravity, fps, background_color, BLACK, RED, GREEN, BLUE, WHITE, GRAY, player_color, square_size, speed, jump
 def image_display(surface, filename, xy):
     img = pygame.image.load(filename)
     surface.blit(img, xy)
@@ -25,7 +25,7 @@ pygame.display.set_caption(GameName)
 done = False
 # pygame.display.set_icon(gameIcon)
 clock = pygame.time.Clock()
-
+velocityY = 0
 while not done:
         clock.tick(fps)
         screen.fill(BLUE)
@@ -34,40 +34,44 @@ while not done:
         background = pygame.draw.rect(screen, BLACK, [0,0,500,500])
         ground_square = pygame.draw.rect(screen, GREEN, [0,300,500,500])
         player_square = pygame.draw.rect(screen, player_color, [playerx,playery,square_size,square_size])
+
+        # Player movement
         if pygame.Rect.colliderect(player_hitbox, ground_square) == 0:
-            playery = playery + 1
+            playery = playery + gravity
+        if not velocityY == 0:
+            playery = playery - velocityY
+            velocityY = velocityY - 1
 
         # Controls
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    if disable_controls == False:
-                        playerx = playerx - speed
-                if event.key == pygame.K_d:
-                    if disable_controls == False:
-                        playerx = playerx + speed
-                if event.key == pygame.K_w:
-                    if disable_controls == False:
-                        playery = playery - jump
-                if event.key == pygame.K_s:
-                    if disable_controls == False:
-                        playery = playery + speed
-                if event.key == pygame.K_LEFT:
-                    if disable_controls == False:
-                        playerx = playerx - speed
-                if event.key == pygame.K_RIGHT:
-                    if disable_controls == False:
-                        playerx = playerx + speed
-                if event.key == pygame.K_UP:
-                    if disable_controls == False:
-                        playery = playery - jump
-                if event.key == pygame.K_DOWN:
-                    if disable_controls == False:
-                        playery =playery + speed
-                if event.key == pygame.K_ESCAPE:
-                    done = True
-                    if show_debug == True:
-                        print("Quit")
+            if event.type == pygame.QUIT:
+                done = True
+        state = pygame.key.get_pressed()
+        if state[pygame.K_a]:
+            if disable_controls == False:
+                playerx = playerx - speed
+        if state[pygame.K_d]:
+            if disable_controls == False:
+                playerx = playerx + speed
+        if state[pygame.K_w]:
+            if disable_controls == False:
+                if pygame.Rect.colliderect(player_hitbox, ground_square) == 1:
+                    playery = playery - jump
+        if state[pygame.K_LEFT]:
+            if disable_controls == False:
+                playerx = playerx - speed
+        if state[pygame.K_RIGHT]:
+            if disable_controls == False:
+                playerx = playerx + speed
+        if state[pygame.K_UP]:
+            if disable_controls == False:
+                if pygame.Rect.colliderect(player_hitbox, ground_square) == 1:
+                    velocityY = jump
+        if state[pygame.K_ESCAPE]:
+            done = True
+            print("Quit")
+
+        print(velocityY)
         print(playerx)
         print(playery)
         pygame.display.update()
