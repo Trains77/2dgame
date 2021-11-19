14b221ccd7ebd2c92c7ca61338ea6cf51e37c8f8
Trains77 <trains77videos123@gmail.com>
2021-11-19 09:37:47 -0500

    Added collision to bottom of squares Improved Floor creation

--- game/game.py
+++ game/game.py
@@ -37,23 +37,31 @@ EnemyVelocity1 = 0
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption(GameName)
 done = False
+enemy1_on_ground = False
+player_on_ground = False
+player_on_roof = False
+enemy1_on_roof = False
 # pygame.display.set_icon(gameIcon)
 clock = pygame.time.Clock()
 velocityY = 0
-def enemy_movement(enemy_pos, EnemyVelocity, enemyid, enemyAlive):
+def enemy_movement(enemy_pos, EnemyVelocity, enemyid, enemyAlive, enemy_on_ground, Enemy_On_Roof):
     finished = done
     enemyid = pygame.draw.rect(screen, BLACK, [enemy_pos[0] - 1, enemy1_pos[1] - 1,square_size + 1,square_size + 1])
     if enemyAlive == True:
-        enemy1_square = pygame.draw.rect(screen, RED, [enemy1_pos[0],enemy1_pos[1],square_size,square_size])
+        enemy1_square = pygame.draw.rect(screen, RED, [enemy_pos[0],enemy_pos[1],square_size,square_size])
     if enemy_pos[0] > playerx:
         enemy_pos[0] = enemy_pos[0] - speed / 2
     if enemy_pos[0] < playerx:
         enemy_pos[0] = enemy_pos[0] + speed / 2
     if enemy_pos[1] > playery:
         if enemy_pos[0] == playerx:
-            if pygame.Rect.colliderect(enemyid, ground_square) == 1:
-                EnemyVelocity = jump * 0.7
-    if pygame.Rect.colliderect(enemyid, ground_square) == 0:
+            if enemy_on_ground == True:
+                if Enemy_On_Roof == False:
+                    EnemyVelocity = jump * 0.8
+    if enemy_on_ground == False:
+        enemy_pos[1] = enemy_pos[1] + gravity
+    if Enemy_On_Roof == True:
+        EnemyVelocity = 0
         enemy_pos[1] = enemy_pos[1] + gravity
     if not EnemyVelocity <= 0:
         enemy_pos[1] = enemy_pos[1] - EnemyVelocity
@@ -64,23 +72,51 @@ def enemy_movement(enemy_pos, EnemyVelocity, enemyid, enemyAlive):
     if enemy_pos[1] > 500:
         enemyAlive = False
     return enemy_pos, EnemyVelocity, enemyAlive, finished
+
+def create_ground(COLOR, x, y, length, width):
+    players_on_ground = player_on_ground
+    enemy1s_on_ground = enemy1_on_ground
+    players_on_roof = player_on_roof
+    enemy1s_on_roof = enemy1_on_roof
+    ground_square = pygame.draw.rect(screen, COLOR, [x,y,length,width])
+    if pygame.Rect.colliderect(player_hitbox, ground_square) == 1:
+        players_on_ground = True
+    if pygame.Rect.colliderect(ground_square, enemy1_hitbox) == 1:
+        enemy1s_on_ground = True
+    if pygame.Rect.colliderect(player_top, ground_square) == 1:
+        players_on_roof = True
+    if pygame.Rect.colliderect(enemy1_top, ground_square) == 1:
+        enemy1s_on_roof = True
+    return players_on_ground, enemy1s_on_ground, players_on_roof, enemy1s_on_roof
 while not done:
         clock.tick(fps)
         screen.fill(BLUE)
         mouse_button_list = pygame.mouse.get_pressed(num_buttons=3)
         player_hitbox = pygame.draw.rect(screen, GRAY, [playerx - 1, playery - 1,square_size + 1,square_size + 1])
-        enemy1_hitbox = pygame.draw.rect(screen, WHITE, [enemy1_pos[0] - 1, enemy1_pos[1] - 1,square_size + 1,square_size + 1])
+        enemy1_hitbox = pygame.draw.rect(screen, WHITE, [enemy1_pos[0], enemy1_pos[1],square_size,square_size])
+        enemy1_top = pygame.draw.rect(screen, RED, [enemy1_pos[0], enemy1_pos[1],square_size, square_size / 4])
+        player_top = pygame.draw.rect(screen, BLUE, [playerx, playery - 1,square_size + 1,square_size / 4])
         background = pygame.draw.rect(screen, BLACK, [0,0,500,500])
-        ground_square = pygame.draw.rect(screen, GREEN, [0,300,500,500])
-        player_square = pygame.draw.rect(screen, player_color, [playerx,playery,square_size,square_size])
-        # Player movement
-        if pygame.Rect.colliderect(player_hitbox, ground_square) == 0:
-            playery = playery + gravity
-        if not velocityY == 0:
-            playery = playery - velocityY
-            velocityY = velocityY - 1
+        player_on_ground, enemy1_on_ground, player_on_roof, enemy1_on_roof = create_ground(GREEN, 50, 450, 400, 10)
+        player_on_ground, enemy1_on_ground, player_on_roof, enemy1_on_roof = create_ground(GREEN, 200, 200, 100, 10)
+        player_on_ground, enemy1_on_ground, player_on_roof, enemy1_on_roof = create_ground(GREEN, 150, 375, 50, 10)
+        player_on_ground, enemy1_on_ground, player_on_roof, enemy1_on_roof = create_ground(GREEN, 225, 300, 50, 10)
 
-        enemy1_pos, EnemyVelocity1, enemy1Alive, done = enemy_movement(enemy1_pos, EnemyVelocity1, enemy1_hitbox, enemy1Alive)
+        player_square = pygame.draw.rect(screen, player_color, [playerx,playery,square_size,square_size])
+
+        # Player movement
+        if player_on_ground == False:
+           playery = playery + gravity
+        if not velocityY == 0:
+            if player_on_roof == False:
+                playery = playery - velocityY
+                velocityY = velocityY - 1
+        if player_on_roof == True:
+            velocityY = 0
+            playery = playery + gravity
+
+
+        enemy1_pos, EnemyVelocity1, enemy1Alive, done = enemy_movement(enemy1_pos, EnemyVelocity1, enemy1_hitbox, enemy1Alive, enemy1_on_ground, enemy1_on_roof)
         # Enemy movement
 
         # Controls
@@ -96,8 +132,9 @@ while not done:
                 playerx = playerx + speed
         if state[pygame.K_w]:
             if disable_controls == False:
-                if pygame.Rect.colliderect(player_hitbox, ground_square) == 1:
-                    velocityY = jump
+                if player_on_ground == True:
+                    if player_on_roof == False:
+                        velocityY = jump
         if state[pygame.K_LEFT]:
             if disable_controls == False:
                 playerx = playerx - speed
@@ -106,8 +143,9 @@ while not done:
                 playerx = playerx + speed
         if state[pygame.K_UP]:
             if disable_controls == False:
-                if pygame.Rect.colliderect(player_hitbox, ground_square) == 1:
-                    velocityY = jump
+                if player_on_ground == True:
+                    if player_on_roof == False:
+                        velocityY = jump
         if state[pygame.K_ESCAPE]:
             done = True
             print("Quit")
@@ -119,3 +157,8 @@ while not done:
             print("Player Cords: " + str(playerx) + ", " + str(playery))
         pygame.display.update()
         pygame.display.flip()
+        # Reset entity falling variable
+        player_on_ground = False
+        enemy1_on_ground = False
+        player_on_roof = False
+        enemy1_on_roof = False
